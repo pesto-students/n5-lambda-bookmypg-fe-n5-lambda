3b6bea94f848e663e272fe293c8586129228e167
Monali <monalidoshi9@gmail.com>
2021-08-05 23:48:13 +0530

    Update design issue for Homepage sidedrawer

--- src/owner/components/content.js
+++ src/owner/components/content.js
@@ -1,88 +1,30 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Grid from "@material-ui/core/Grid";
+import React from "react";
+import ReactDOM from "react-dom";
+import Table from "@material-ui/core/Table/Table";
+import TableHead from "@material-ui/core/TableHead/TableHead";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+import Paper from "@material-ui/core/Paper/Paper";
+import ResponsiveDrawer from "./responsivedrawer";
+import Grid from "@material-ui/core/Grid/Grid";
+import Link from "@material-ui/core/Link";
+import TablePagination from "@material-ui/core/TablePagination";
+import Switch from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Sidedrawer from "./sidedrawer";
+import TextField from "@material-ui/core/TextField";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
-import { useHistory } from "react-router-dom";
-
-const useStylesselect = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(2),
-    minWidth: "80%",
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6, 0, 4),
-    paddingTop: "100px",
-    maxWidth: "600px",
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-    maxWidth: "1400px",
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-  topPadding: {
-    marginTop: "80px",
-  },
-}));
-
-export default function Album(props) {
-  const classes = useStyles();
-
-  const classesselect = useStylesselect();
-
-  const [location, setLocation] = React.useState("");
-  const history = useHistory();
-
-  const handleChange = (event) => {
-    setLocation(event.target.value);
-    history.push("/propertylist");
-  };
+export default function Tablefile() {
   return (
-    <React.Fragment>
-      <Sidedrawer />
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4} className={classes.topPadding}>
-          <Grid item xs={12} sm={6}>
-            <p>Hi</p>
-          </Grid>
-        </Grid>
-      </Container>
-    </React.Fragment>
+    <div className="Table">
+      <ResponsiveDrawer>
+        <h1>Hi</h1>
+      </ResponsiveDrawer>
+    </div>
   );
 }
