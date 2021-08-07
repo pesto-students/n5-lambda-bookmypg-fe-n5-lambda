37ba67fe60dd0286a3ec9d361ab15ef0e3f6ef28
haridhole <hari.dhole@circles.asia>
2021-08-08 01:39:51 +0530

    implemented swiperjs

--- src/user/components/similarproperties.js
+++ src/user/components/similarproperties.js
@@ -16,7 +16,9 @@ import Cardrating from "../components/cardratings";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import { useHistory } from "react-router-dom";
 import { S3_BUCKET_URL } from "../../constant";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/swiper.min.css";
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
@@ -103,95 +105,181 @@ export default function Album(props) {
     setActiveStep((prevActiveStep) => prevActiveStep - 4);
   };
 
-  return (
-    <React.Fragment>
-      <Typography component="h1" variant="h6" color="textPrimary" gutterBottom>
-        {props.title}
-      </Typography>
-      <Grid className={classes.responsivegrid}>
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          disabled={activeStep === 0}
-          onClick={handleBack}
-          className={classes.icons}
-          style={{ backgroundColor: "transparent", padding: "0px" }}
-          disableRipple
+  const [state, setState] = React.useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+  const { mobileView, drawerOpen } = state;
+  React.useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <React.Fragment>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="textPrimary"
+          gutterBottom
         >
-          <NavigateBeforeIcon />
-        </IconButton>
-        <Grid container spacing={5}>
-          {props.properties &&
-            props.properties.length &&
-            props.properties.map((property) => (
-              <Grid item key={property._id} xs={12} sm={6} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={`${S3_BUCKET_URL}/${property.photos[0]}`}
-                    title={property.photos[0]}
-                  />
-                  <CardContent className={classes.cardContent}>
+          {props.title}
+        </Typography>
+        <Grid className={classes.responsivegrid}>
+          <Swiper slidesPerView={4}>
+            <Grid container spacing={5}>
+              {props.properties &&
+                props.properties.length &&
+                props.properties.map((property) => (
+                  <Grid item key={property._id} xs={12} sm={6} md={3}>
+                    <SwiperSlide>
+                      <Card className={classes.card}>
+                        <CardMedia
+                          className={classes.cardMedia}
+                          image={`${S3_BUCKET_URL}/${property.photos[0]}`}
+                          title={property.photos[0]}
+                        />
+                        <CardContent className={classes.cardContent}>
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <Typography
+                              gutterBottom
+                              variant="h6"
+                              component="h2"
+                            >
+                              {property.name}
+                            </Typography>
+                            <Typography gutterBottom variant="caption">
+                              <LocationOnIcon />
+                              {property.location.name}
+                            </Typography>
+                          </div>
+                          <Typography gutterBottom variant="body2">
+                            {property.description}
+                          </Typography>
+                        </CardContent>
+                        <div
+                          style={{
+                            display: "flex",
+                            paddingBottom: "10px",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <Cardrating
+                            value={data.rating}
+                            number={property.numreviews || 0}
+                          />
+
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="ContainedSecondary"
+                            style={{ textTransform: "none" }}
+                            onClick={() =>
+                              history.push(`/property-details/${property._id}`)
+                            }
+                          >
+                            More Details
+                          </Button>
+                        </div>
+                      </Card>
+                      <div></div>
+                    </SwiperSlide>
+                  </Grid>
+                ))}
+            </Grid>
+          </Swiper>
+        </Grid>
+      </React.Fragment>
+    );
+  };
+
+  const displayMobile = () => {
+    return (
+      <React.Fragment>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="textPrimary"
+          gutterBottom
+        >
+          {props.title}
+        </Typography>
+        <Grid className={classes.responsivegrid}>
+          <Grid container spacing={5}>
+            {props.properties &&
+              props.properties.length &&
+              props.properties.map((property) => (
+                <Grid item key={property._id} xs={12} sm={6} md={3}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={`${S3_BUCKET_URL}/${property.photos[0]}`}
+                      title={property.photos[0]}
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Typography gutterBottom variant="h6" component="h2">
+                          {property.name}
+                        </Typography>
+                        <Typography gutterBottom variant="caption">
+                          <LocationOnIcon />
+                          {property.location.name}
+                        </Typography>
+                      </div>
+                      <Typography gutterBottom variant="body2">
+                        {property.description}
+                      </Typography>
+                    </CardContent>
                     <div
                       style={{
                         display: "flex",
+                        paddingBottom: "10px",
                         justifyContent: "space-between",
                       }}
                     >
-                      <Typography gutterBottom variant="h6" component="h2">
-                        {property.name}
-                      </Typography>
-                      <Typography gutterBottom variant="caption">
-                        <LocationOnIcon />
-                        {property.location.name}
-                      </Typography>
-                    </div>
-                    <Typography gutterBottom variant="body2">
-                      {property.description}
-                    </Typography>
-                  </CardContent>
-                  <div
-                    style={{
-                      display: "flex",
-                      paddingBottom: "10px",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Cardrating
-                      value={data.rating}
-                      number={property.numreviews || 0}
-                    />
+                      <Cardrating
+                        value={data.rating}
+                        number={property.numreviews || 0}
+                      />
 
-                    <Button
-                      size="small"
-                      variant="contained"
-                      color="ContainedSecondary"
-                      style={{ textTransform: "none" }}
-                      onClick={() =>
-                        history.push(`/property-details/${property._id}`)
-                      }
-                    >
-                      More Details
-                    </Button>
-                  </div>
-                </Card>
-              </Grid>
-            ))}
+                      <Button
+                        size="small"
+                        variant="contained"
+                        color="ContainedSecondary"
+                        style={{ textTransform: "none" }}
+                        onClick={() =>
+                          history.push(`/property-details/${property._id}`)
+                        }
+                      >
+                        More Details
+                      </Button>
+                    </div>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
         </Grid>
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="span"
-          component="span"
-          disabled={activeStep === 16}
-          onClick={handleNext}
-          className={classes.icons}
-          style={{ backgroundColor: "transparent" }}
-          disableRipple
-        >
-          <NavigateNextIcon />
-        </IconButton>
-      </Grid>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  };
+  return <div>{mobileView ? displayMobile() : displayDesktop()}</div>;
 }
