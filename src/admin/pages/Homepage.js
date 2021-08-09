cc521e36c731d014cea2ed7d17c2d42614782ba2
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-09 17:05:55 +0530

    Updated Admin Dashboard
    Added Chartjs to Admin Module

--- src/admin/pages/Homepage.js
+++ src/admin/pages/Homepage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import Header from "../components/header";
-import Content from "../components/home/Content";
-import Footer from "../../components/footer/footer";
+import Content from "../components/content";
+import Footer from "../components/footer";
 import Theme from "../theme/theme";
+import OverticalBar from "../Ocharts/OverticalBar";
+import OpieChart from "../Ocharts/Opiechart";
+import Grid from "@material-ui/core/Grid";
 
 export default function HomePage() {
   const [loggedUser, setLoggedUser] = React.useState("");
@@ -13,6 +17,30 @@ export default function HomePage() {
       <MuiThemeProvider theme={Theme}>
         <Header loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
         <Content />
+        <Grid container justify={"space-between"}>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            style={{
+              marginLeft: "20em",
+              textAlign: "center",
+            }}
+          >
+            <OverticalBar />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={5}
+            style={{
+              marginLeft: "20em",
+              textAlign: "center",
+            }}
+          >
+            <OpieChart />
+          </Grid>
+        </Grid>
         <Footer />
       </MuiThemeProvider>
     </div>
