bd60704229556ca17f3a9eeee235a1ac5d29f7cc
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-17 19:35:03 +0530

    Modified Charts

--- src/admin/components/home/Content.js
+++ src/admin/components/home/Content.js
@@ -3,7 +3,7 @@ import ResponsiveDrawer from "../responsivedrawer/ResponsiveDrawer";
 
 export default function Tablefile() {
   return (
-    <div className="Table">
+    <div className="Table" data-testid="content">
       <ResponsiveDrawer>
         <h1>Hi</h1>
       </ResponsiveDrawer>
