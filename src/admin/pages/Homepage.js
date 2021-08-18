fed9a7ce33f5391d750a32b138ca8dd3b23c1952
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-18 14:10:22 +0530

    Add files via upload

---
+++ src/admin/pages/Homepage.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Header from "components/header/header";
+import Content from "../components/home/Content";
+import Footer from "../../components/footer/footer";
+import Theme from "../theme/theme";
+import OverticalBar from "../Ocharts/OverticalBar";
+import OpieChart from "../Ocharts/Opiechart";
+import Grid from "@material-ui/core/Grid";
+const headersData = [
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const responsiveHeaderData = [
+  {
+    label: "Home",
+    href: "/home",
+  },
+  {
+    label: "Owners",
+    href: "/owner-list",
+  },
+  {
+    label: "Amenities",
+    href: "/amenity-list",
+  },
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const listitems = [
+  {
+    label: "My Profile",
+    href: "/MyProfile",
+  },
+  {
+    label: "Logout",
+    href: "/",
+  },
+];
+
+export default function AmenityList() {
+  const [loggedUser, setLoggedUser] = React.useState("");
+
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Header
+          loggedUser={loggedUser}
+          setLoggedUser={setLoggedUser}
+          headersData={headersData}
+          responsiveHeaderData={responsiveHeaderData}
+          listitems={listitems}
+        />
+        <Content />
+        <Grid container justify={"space-between"}>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            style={{
+              marginLeft: "20em",
+              textAlign: "center",
+            }}
+          >
+            <OverticalBar />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={5}
+            style={{
+              marginLeft: "20em",
+              textAlign: "center",
+            }}
+          >
+            <OpieChart />
+          </Grid>
+        </Grid>
+        <Footer />
+      </MuiThemeProvider>
+    </div>
+  );
+}
