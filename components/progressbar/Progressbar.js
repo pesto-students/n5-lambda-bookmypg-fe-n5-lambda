c15001eb73bda58b069cd9e49736211b0ccff6ed
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-18 14:09:15 +0530

    Delete components directory

--- components/progressbar/Progressbar.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import BorderLinearProgress from "./Progressbar.styles";
-
-export default function ReviewBars(props) {
-  return <BorderLinearProgress variant="determinate" value={props.value} />;
-}
