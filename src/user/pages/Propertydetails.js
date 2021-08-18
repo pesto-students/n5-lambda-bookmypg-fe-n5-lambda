346f3bedf58039823db8f54065890db904f6e998
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-18 14:17:00 +0530

    Add files via upload

---
+++ src/user/pages/Propertydetails.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { connect } from "react-redux";
+import Grid from "@material-ui/core/Grid";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Propertycontent from "../components/property/PropertyContent";
+import Header from "../components/header/header";
+import Footer from "components/footer/footer";
+import Theme from "../theme/theme";
+import PropertiesSelector from "../helpers/PropertiesSelector";
+import propertiesActions from "../../redux-store/actions/propertiesActions";
+
+export function PropertyDetails(props) {
+  const property_id = props.match.params.id;
+
+  const property = props.properties.filter(
+    (property) => property._id === property_id
+  )[0];
+
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Grid container alignItems="center">
+          <Header />
+          <Propertycontent
+            property={property}
+            properties={props.properties}
+            latestProperties={props.latestProperties}
+          />
+          <Footer type="center" />
+        </Grid>
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  const propertiesSelector = PropertiesSelector(state.properties);
+
+  return {
+    properties: propertiesSelector.getPropertiesData().data,
+    latestProperties: propertiesSelector.getLatestPropertiesData().data,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProperties: () => dispatch(propertiesActions.getProperties()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PropertyDetails);
---
+++ src/user/pages/myprofile/Paymentlist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Header from "../../components/header/header";
+import Content from "../../components/payment/PaymentListContent";
+import Footer from "components/footer/footer";
+import Theme from "../../theme/theme";
+
+export default function PaymentListContent() {
+  const [loggedUser, setLoggedUser] = React.useState("");
+
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Header loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
+        <Content />
+        <Footer />
+      </MuiThemeProvider>
+    </div>
+  );
+}
