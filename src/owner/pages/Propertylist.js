30a8b36561374844d3adceeca16f9f7ff5401506
Vignesh Shenoy <gamerx270@gmail.com>
2021-08-18 14:14:09 +0530

    Add files via upload

---
+++ src/owner/pages/Propertylist.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Header from "components/header/header";
+import Content from "../components/property/PropertyListContent";
+import Footer from "../../components/footer/footer";
+import Theme from "../theme/theme";
+const headersData = [
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const responsiveHeaderData = [
+  {
+    label: "Home",
+    href: "/home",
+  },
+  {
+    label: "My Properties",
+    href: "/owner-property-list",
+  },
+  {
+    label: "Tenants",
+    href: "/tenant-list",
+  },
+  {
+    label: "Complaints",
+    href: "/owner-complaint-list",
+  },
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const listitems = [
+  {
+    label: "My Profile",
+    href: "/MyProfile",
+  },
+  {
+    label: "Logout",
+    href: "/",
+  },
+];
+
+export default function PropertyList() {
+  const [loggedUser, setLoggedUser] = React.useState("");
+
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Header
+          loggedUser={loggedUser}
+          setLoggedUser={setLoggedUser}
+          headersData={headersData}
+          responsiveHeaderData={responsiveHeaderData}
+          listitems={listitems}
+        />
+        <Content />
+        <Footer />
+      </MuiThemeProvider>
+    </div>
+  );
+}
---
+++ src/owner/pages/Tenantlist.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Header from "components/header/header";
+import Content from "../components/tenant/TenantListContent";
+import Footer from "../../components/footer/footer";
+import Theme from "../theme/theme";
+
+const headersData = [
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const responsiveHeaderData = [
+  {
+    label: "Home",
+    href: "/home",
+  },
+  {
+    label: "My Properties",
+    href: "/myproperties",
+  },
+  {
+    label: "Tenants",
+    href: "/tenants-list",
+  },
+  {
+    label: "Complaints",
+    href: "/complaints",
+  },
+  {
+    label: "About us",
+    href: "/about",
+  },
+  {
+    label: "Contact us",
+    href: "/contact",
+  },
+];
+
+const listitems = [
+  {
+    label: "My Profile",
+    href: "/MyProfile",
+  },
+  {
+    label: "Logout",
+    href: "/",
+  },
+];
+
+export default function TenantList() {
+  const [loggedUser, setLoggedUser] = React.useState("");
+
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Header
+          loggedUser={loggedUser}
+          setLoggedUser={setLoggedUser}
+          headersData={headersData}
+          responsiveHeaderData={responsiveHeaderData}
+          listitems={listitems}
+        />
+        <Content />
+        <Footer />
+      </MuiThemeProvider>
+    </div>
+  );
+}
