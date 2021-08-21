08f46701a4a655d057fc7e3e5472f4111debcf0a
Monali <monalidoshi9@gmail.com>
2021-08-21 11:19:34 +0530

    Tried ellipsis

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -15,6 +15,7 @@ import Box from "@material-ui/core/Box";
 import MoreButton from "@material-ui/core/Button";
 import Rating from "user/components/rating/rating";
 import ScheduleVisit from "user/components/schedulevisit/ScheduleVisit";
+import LinesEllipsis from "react-lines-ellipsis";
 
 export default function ButtonComponent(props) {
   const classes = useStyles();
